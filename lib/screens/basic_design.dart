ba89d09ddc39e61ecd1a3eeb9123a1a1f570af92
tecnologyboy <lps76@hotmail.com>
2023-04-24 23:01:48 -0400

    casi termino, solo falta agregar el comentario y colocar lo botones en un Widget Separado

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -8,8 +8,108 @@ class BasicDesignScreen extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: const [
+        //Header Image
         Image(image: AssetImage('assets/landscape.jpg')),
+
+        //Ttile
+        Title(),
+
+        //Button Section
+        ButtonSection()
       ],
     ));
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Kandersteg, Swtzerland',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.phone,
+                  color: Colors.blue,
+                ),
+              ),
+              const Text('CALL', style: TextStyle(color: Colors.blue))
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.directions,
+                  color: Colors.blue,
+                ),
+              ),
+              const Text('ROUTE', style: TextStyle(color: Colors.blue))
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.share,
+                  color: Colors.blue,
+                ),
+              ),
+              const Text(
+                'SHARE',
+                style: TextStyle(color: Colors.blue),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
